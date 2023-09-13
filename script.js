1c8d75597f43e258e8f36458c7bb2a5fcb80d2ff
Vitorio Tuckers <vitoriofibla@hotmail.com>
2023-09-13 08:36:02 -0300

    Changelog -Removed all sphagetti code. -Added base logic for inserting values and displaying them. -Added clear and all clear function.

--- script.js
+++ script.js
@@ -1,101 +1,36 @@
-const numbers = document.querySelectorAll('.num');
-const operator = document.querySelectorAll('.operator');
-const clear = document.querySelectorAll('.clear');
-const currentOperationDisplay = document.getElementById('current-operation');
-const previousOperationDisplay = document.getElementById('previous-operation');
+const calculatorButtons = document.querySelector('.calculator-buttons');
+const currentOperationDisplay = document.querySelector('#current-operation');
+const previousOperationDisplay = document.querySelector('#previous-operation');
 
 let currentOperation = [];
-let operationQueue = [];
-let operatorIncluded = false;
+let previousOperation = [];
 
-const operatorsList = {
-  '+': (firstValue, secondValue) => {
-    return firstValue + secondValue;
-  },
-  '-': (firstValue, secondValue) => {
-    return firstValue - secondValue;
-  },
-  '*': (firstValue, secondValue) => {
-    return firstValue * secondValue;
-  },
-  '/': (firstValue, secondValue) => {
-    return firstValue / secondValue;
-  },
-  '%': (firstValue, secondValue) => {
-    return firstValue % secondValue;
-  },
-};
-
-function calculation(firstValue, operator, secondValue) {
-  if (operator in operatorsList) {
-    return operatorsList[operator](firstValue, secondValue);
-  }
-}
-
-function updateDisplay() {
-  previousOperationDisplay.textContent = operationQueue.join(' ');
-  currentOperationDisplay.textContent = currentOperation.join('');
-}
-
-function convertInputToFloat() {
-  operationQueue.push(parseFloat(currentOperation.join('')));
-  currentOperation.length = 0;
+function insertNumber(number) {
+  currentOperation.push(number);
 }
 
 function insertOperator(operator) {
-  operationQueue.push(operator);
-  operatorIncluded = true;
+  currentOperation.push(operator);
 }
 
-for (let button of clear) {
-  button.addEventListener('click', () => {
-    if (button.id == 'C') {
-      currentOperation.pop();
-    } else {
-      currentOperation.length = 0;
-      operationQueue.length = 0;
-    }
-    updateDisplay();
-  });
+function clear(input) {
+  if (input == 'C') {
+    currentOperation.pop();
+  } else {
+    currentOperation.length = 0;
+    previousOperation.length = 0;
+  }
 }
 
-for (let button of numbers) {
-  button.addEventListener('click', () => {
-    if (operationQueue.length != 1) {
-      if (button.id === '.') {
-        if (currentOperation.includes('.')) {
-          return;
-        } else if (currentOperation.length === 0) {
-          currentOperation.push('0');
-        }
-        if (currentOperation.join('').includes('.')) {
-          return;
-        }
-      }
-      currentOperation.push(button.id);
-      updateDisplay();
-    }
-  });
-}
-
-for (let button of operator) {
-  button.addEventListener('click', () => {
-    if (currentOperation.length >= 1 && operationQueue.length != 2) {
-      convertInputToFloat();
-      insertOperator(button.id);
-    } else if (operatorIncluded && currentOperation.length >= 1) {
-      if (button.id === '=') {
-        convertInputToFloat();
-        operationQueue = [calculation(...operationQueue)];
-        operatorIncluded = false;
-      } else if (button.id !== '=') {
-        convertInputToFloat();
-        operationQueue = [calculation(...operationQueue)];
-        insertOperator(button.id);
-      }
-    } else if (button.id !== '=' && operationQueue.length == 1) {
-      insertOperator(button.id);
-    }
-    updateDisplay();
-  });
-}
+calculatorButtons.addEventListener('click', e => {
+  console.log(e.target.className);
+  if (e.target.className == 'num') {
+    insertNumber(e.target.id);
+  } else if (e.target.className == 'operator') {
+    insertOperator(e.target.id);
+  } else {
+    clear(e.target.id);
+  }
+  currentOperationDisplay.textContent = currentOperation;
+  previousOperationDisplay.textContent = previousOperation;
+});
